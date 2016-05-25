c726a6145f44f966076eb298e72646dd36e0174b
Chris Ritter <bayreuth@me.com>
2016-05-25 20:04:08 +1000

    Initial Commit

---
+++ Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Chris Ritter on 25/05/2016.
+//  Copyright © 2016 Ridaste Software Enterprises. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
