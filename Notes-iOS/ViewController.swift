4d8934cba961e5177193592ed70e32ad456f1373
Chris Ritter <bayreuth@me.com>
2016-06-08 23:13:01 +1000

    commit-8

---
+++ Notes-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes-iOS
+//
+//  Created by Chris Ritter on 1/06/2016.
+//  Copyright © 2016 Ridaste Software Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
